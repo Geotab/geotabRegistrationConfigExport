55fdd58ab95d18791137b0cbd8c574c6ad23fcba
Brett Kelley <brettkelley@geotab.com>
2020-01-20 14:51:36 +0100

    added set addins to null method and commented unnecessary zones length

--- _dev/sources/addin.ts
+++ _dev/sources/addin.ts
@@ -445,7 +445,7 @@ class Addin {
                 this.setAddinsToNull();
             }
             let mapProvider = this.miscBuilder.getMapProviderName(this.data.misc.mapProvider.value);
-            this.data.zones.length
+            // this.data.zones.length
             this.showEntityMessage(groupsBlock, this.data.groups.length - 1, "group");
             this.showEntityMessage(securityClearancesBlock, this.data.securityGroups.length, "security clearance");
             this.showEntityMessage(rulesBlock, this.data.rules.length, "rule");
